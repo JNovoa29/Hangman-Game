9914583537243b977cd40b60534a2b6617de5c18
Jordan Novoa <jordan.novoa15@gmail.com>
2018-02-04 18:55:02 -0800

    rehaul js to jq

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,85 @@
-// Hangman!
-
-var letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-// console.log(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
-
-//
-var gameOfThrones =["longclaw", "khaleesi", "dragons", "winter", "lannister", "bastard", "baratheon", "stark"]
-
-var numWins = 0
-var numLosses = 0
-var guessTally = 0
-var maxGuesses = 6
-var randWord = gameOfThrones[Math.floor(Math.random() * gameOfThrones.length)]
-// var usedLetters = letterChoices[Math.floor(Math.random() * letterChoices.length)]
-
-console.log(randWord)
-// console.log(usedLetters)
-
-// on key events
-// document.onkeyup = function (event) {
-
-//     var userGuess = event.key 
-
-//     // if (randWord === "Longclaw") {
-//     //     letterChoices === "l", "o", "n", "g", "c", "a", "w"
-//     // }
-
-//     // else {
-//     //     alert("Your Watch Has Ended")
-//     // }
-
-//     console.log(event)
+// // Hangman!
 
 
-// }
+// // console.log(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
 
-var answerArray= []
-for (var i=0; i <randWord.length; i++) {
-    answerArray[i] = "_";
-}
+// //
 
-var remainingLetters = randWord.length;
+// // var guessLeft = document.getElementById("guessesleft")
 
-while(remainingLetters > 0) {
-    alert(answerArray.join(""))
-    var guess = prompt("Guess a letter or click cancel to stop playing")
+// // examples to add text pulling from html
+// //  var myString = "Some Text"
+// //     myString.innerHTML = "<h1>" + myString + "</h1>"
+// // var newDiv = document.createElement("div")
+// // newDiv.innerHTML = "<h3> Some more text for you </h3>"
+// // targetDiv.appendChild(newDiv) 
+// // referencing css 
+// // targetDiv.setAttribute("class", "changeColor") 
 
-    if(guess === null) {
-        break;
-    }
-    else if(guess.length !==1) {
-        alert("Type to start playing!")
-    }
-    else {
-        for (var j = 0; j < randWord.length; j++) {
-            if (randWord[j] === guess) {
-                answerArray[j] = guess
-                remainingLetters--
-            }
-        }
-    }
-}
+// // console.log(usedLetters)
+
+// // on key events
+// // document.onkeyup = function (event) {
+
+// //     var userGuess = event.key 
+
+// //     // if (randWord === "Longclaw") {
+// //     //     letterChoices === "l", "o", "n", "g", "c", "a", "w"
+// //     // }
+
+// //     // else {
+// //     //     alert("Your Watch Has Ended")
+// //     // }
+
+// //     console.log(event)
+
+
+// // }
+// document.addEventListener("DOMContentLoaded", function() {
+//     var letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//     var gameOfThrones =["longclaw", "greyjoy", "tyrell", "hodor", "khaleesi", "dragons", "dothraki", "winter", "lannister", "bastard", "baratheon", "stark"]
+//     var numWins = 0
+//     var guessLeft = 0
+//     var lettersGuessed = 8
+//     var randWord = gameOfThrones[Math.floor(Math.random() * gameOfThrones.length)]
+    
+//     console.log(randWord)
+       
+   
+   
+//     var answerArray= []
+//     for (var i=0; i <randWord.length; i++) {
+//         answerArray[i] = "_";
+//     }
+    
+//     var remainingLetters = randWord.length;
+    
+//     while(remainingLetters > 0) {
+//         //press a key to start the game
+//         alert(answerArray.join(""))
+//     //takes player guess and saves to variable guess below depending on player choice
+//         var guess = prompt("Guess a letter or click cancel to stop playing")
+//     //cancel button
+//         if(guess === null) {
+//             break;
+//         }
+//         //guess exceeds one letter
+//         else if(guess.length !==1) {
+//             alert("Only one letter...")
+//         }
+//         //update game with guess entered
+//         else {
+//             for (var j = 0; j < randWord.length; j++) {
+//                 if (randWord[j] === guess) {
+//                     answerArray[j] = guess
+//                     remainingLetters--
+//                 }
+//                 else {
+                    
+//                 }
+                
+//             }
+//         }
+//     }
+//     });
 
---
+++ assets/javascript/game1.js
@@ -0,0 +1,198 @@
+$(document).ready(function () {
+
+    var wins = 0
+    var maxGuess = 8
+    var maxGuessCount = 0
+    var gotWords = ["longclaw", "greyjoy", "tyrell", "hodor", "khaleesi", "dragons", "dothraki", "winter", "lannister", "bastard", "baratheon", "stark"]
+
+    var rightLtrsArr = []
+    var randWord
+    var randWordArr = []
+    var tempSpace = ""
+    var positions = [], i
+    var randWordLngth
+    var allGuesses = []
+    var guessedWord
+    var splitRandWordArr = []
+
+    //Game Begins
+
+    function startGame() {
+
+        //number of wins
+        $("winCountBox").html(wins)
+        //clear wins
+        $("#wrongGuessList").empty()
+
+        //call new word from the array
+        $("#userInput").attr("disabled", true)
+
+        //clear guesses counted
+        $("#guessesLeftBox").empty()
+
+        //clear current word
+        $("#wordBox").empty()
+
+        //clear previous letters guessed
+        allGuesses = []
+    }
+
+
+
+    //Start the Game over
+
+    function resetGame() {
+        //reset wins back to zero
+        wins = 0
+
+        //return to start game
+        startGame()
+    }
+
+    //Grabbing a word from the array
+
+    function getWord() {
+        randWordArr.length = 0;
+
+        startGame()
+
+        //grabbing new word
+        randWord = gotWords[Math.floor(Math.random() * gotWords.length)]
+        randWordLngth = randWord.length
+        randWordArr.push(randWord)
+        console.log("rand word arr is " + randWordArr)
+
+        //black spaces for the random word pulled
+        tempSpace = ""
+        for (var i = 0; i < randWordLngth; i++) {
+            tempSpace = tempSpace + "_"
+        }
+
+        $("#wordBox").html(tempSpace)
+
+        //returns max geusses and number of guesses entered to 0
+
+        guessCount = 0
+        maxGuessCount
+        $("#guessesLeftBox").html("0 / " + maxGuessCount)
+
+        //places incorrect guesses into box for player view
+        $("#wrongGuessList").empty()
+
+        //enter letters into fields
+        $("#userInput").attr("disabled", false)
+
+        //removes correctly guessed letter from the array
+        rightLtrsArr = []
+    }
+
+    //all indexes within the array
+    function getAllIndexes(word, val) {
+        positions = []
+        for (i = 0; i < word.length; i++) {
+            if (word[i] === val) {
+                positions.push(i)
+            }
+        }
+    }
+    // start game
+
+    $("#getWordBTN").on("click", function () {
+        startGame()
+    })
+
+    //replace function 
+
+    String.prototype.replaceAt = function (index, replacement) {
+        return this.substr(0, index) + replacement + this.substr(index + replacement.length)
+    }
+
+    // reset button
+    $(".resetGameBTN").on("click", function () {
+        resetGame()
+    })
+
+    //new word
+    $("#getWordBTN").on("click", function () {
+        getWord()
+    })
+
+
+    $(document).keypress(function (e) {
+        if (e.which == 13) {
+            $(".inputBTN").on()
+        }
+        console.log(keypress())
+    })
+
+    //checks and balances in events
+    $("#inputBTN").on("click", function () {
+
+        //if user enters capital, change to lowercase
+        var userInput = $("input").val().toLowerCase()
+        $("input").val("")
+
+        //verify a single letter has been entered, error for multiple inputs
+        function isSingleInput(userInput) {
+            if (userInput.length === 1) {
+                //verify string
+                function isNumber(x) {
+                    if (isNaN(x)) {
+                        //check if already guessed
+                        if (allGuesses.indexOf(userInput) >= 0) {
+                            alert("A player forgets a letter has already been guessed...")
+                            return
+                        }
+                        else {
+                            allGuesses.push(userInput)
+                        }
+
+                        //check if input is inside the random word array
+                        var isCharInWord = randWord.indexOf(userInput)
+
+                        if (isCharInWord >= 0) {
+
+                            if ($.inArray(userInput, randWordArr)) {
+
+                                getAllIndexes(randWord, userInput)
+
+                                //positions of correctly guessed letters
+                                for (var i = 0; i < positions.length; i++) {
+                                    var x = parseInt(positions[i])
+                                    tempSpace = tempSpace.replaceAt(x, userInput)
+                                    rightLtrsArr.push(userInput)
+                                }
+
+                                //show spaces on screen
+                                $("#wordBox").html(tempSpace)
+
+                                //word check
+                                splitRandWordArr = randWord.split("")
+                                splitRandWordArr.sort()
+                                rightLtrsArr.sort()
+
+                                if (rightLtrsArr.length === splitRandWordArr.length) {
+                                    $("#userInput").attr("disabled", true)
+                                    alert("Victorious! The Iron Throne is yours... for now.")
+                                    wins++
+                                    $("#winCountBox").html(wins)
+                                }
+                            }
+                        }
+                        else {
+                            //wrong guesses
+                            $("#wrongGuessList").append("<li class='list-group-item'>" + userInput + "</li>")
+                            guessCount++
+                            $("guessesLeftBox").html(guessCount + " / " + maxGuess)
+
+                            //out of tries
+                            if (guessCount === maxGuess) {
+                                alert("What is dead may never die. Generate a new word and try again.")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+})  
